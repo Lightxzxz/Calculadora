63dbe0aca029d03fd78f594df23f4762e5f62c11
Lightxz <Pedro.7700151@gmail.com>
2025-09-04 21:15:13 -0300

    Implementação da classe adição e subtração

--- src/Adicao.java
+++ src/Adicao.java
@@ -1,5 +1,5 @@
-public class Subtracao {
+public class Adicao {
     public double calcular(double a, double b) {
-        return a - b;
+        return a + b;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Calculadora Simples ===");
+        System.out.println("Escolha a operação:");
+        System.out.println("1 - Soma (+)");
+        System.out.println("2 - Subtração (-)");
+        System.out.println("3 - Multiplicação (*)");
+        System.out.println("4 - Divisão (/)");
+        System.out.print("Opção: ");
+        int opcao = sc.nextInt();
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = sc.nextDouble();
+
+        double resultado = 0;
+
+        switch (opcao) {
+            case 1:
+                Adicao ad = new Adicao();
+                resultado = ad.calcular(num1, num2);
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 2:
+                Subtracao sub = new Subtracao();
+                resultado = sub.calcular(num1, num2);
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 3:
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado: " + resultado);
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                }
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+
+        sc.close();
     }
 }
